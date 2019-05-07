7c52f9d2216ccf36af9b11cae7f9e13caf007691
Giovanni <giovanni.alberto@hotmail.com>
2019-05-07 10:43:57 +0100

    developing output

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+from __future__ import division
 import csv
 from data import Semeval
 import similarities as sim
+import pandas as pd
+
 
 f = open('../stsbenchmark/sts-test.csv')
+
+w = open('./out.txt', 'w')
 
 reader = csv.reader(f, delimiter='\t', quoting=csv.QUOTE_NONE)
 
@@ -32,7 +37,86 @@ for row in reader:
 # sim.liuWangSimilarity(synsets1, synsets2)
 #
 
-for x in data:
-    aguirre = sim.aguirreSimilarity(x.sentence1, x.sentence2)
+# for x in data:
+#     aguirre = sim.aguirreSimilarity(x.sentence1, x.sentence2)
+#
+#     w.write("Sentence 1: " + ' '.join(x.sentence1) + " | Sentence 2: " + ' '.join(x.sentence2) + " -> " + str(aguirre) + "\n")
 
-    print ("Sentence 1: " + ' '.join(x.sentence1) + " | Sentence 2: " + ' '.join(x.sentence2) + " -> " + str(aguirre))
+def normalize(data, ori_min, ori_max, min, max):
+
+    ori = ori_max - ori_min
+    obj = max - min
+
+    valueScaled = float(float(data) - ori_min) / float(ori)
+
+    return valueScaled
+
+
+
+def calcData(data, method):
+
+    count = 0
+    mean = 0.0
+
+    success = 0
+    margin = 0.1
+
+    for d in data:
+        count +=1
+
+        if method == 'real':
+            mean += float(d.score)
+
+        elif method == 'aguirre':
+            aguirre = sim.aguirreSimilarity(d.sentence1, d.sentence2)
+            if float(d.score) - margin < aguirre < float(d.score) + margin:
+                success += 1
+            mean += aguirre
+
+        elif method == 'sim_path':
+            sim_path = sim.liuWangSimilarity(d.synsets1, d.synsets2, method)
+            if float(d.score) - margin < sim_path < float(d.score) + margin:
+                success += 1
+            mean += sim_path
+
+        elif method == 'wup':
+            sim_path = sim.liuWangSimilarity(d.synsets1, d.synsets2, method)
+            if float(d.score) - margin < sim_path < float(d.score) + margin:
+                success += 1
+            mean += sim_path
+
+    return count, mean/count, success
+
+def fillDataFrame(data, dataset, method='real'):
+
+    count, mean, success = calcData(data, method)
+
+    dataset['Count'].append(count)
+    dataset['Mean'].append(mean)
+    dataset['Success'].append(success)
+    dataset['Success Perc'].append(success / count * 100.0)
+
+    return dataset
+
+
+for d in data:
+    d.score = normalize(d.score, 0, 5, 0, 1)
+
+methods = ['real', 'aguirre', 'sim_path', 'wup']
+
+dataset = {'Method': methods,
+           'Count': [],
+           'Mean': [],
+           'Success' : [],
+           'Success Perc' : []}
+
+
+for method in methods:
+    dataset = fillDataFrame(data, dataset, method)
+
+df = pd.DataFrame(dataset)
+
+print(df[['Method', 'Count', 'Mean', 'Success', 'Success Perc']])
+
+
+
--- similarities.py
+++ similarities.py
@@ -66,26 +66,23 @@ def aguirreSimilarity(sentence1, sentence2):
         if word in sentence2:
             aligned += 1.0
 
-    return (2*aligned)/(len(sentence1) + len(sentence2))
+    try:
+        return (2*aligned)/(len(sentence1) + len(sentence2))
+    except:
+        return 0.0
 
 
-def liuWangSimilarity(synsets1, synsets2):
+def liuWangSimilarity(synsets1, synsets2, method):
 
     T = union(synsets1, synsets2)
 
-    print(T)
-
-    V1 = calculateVector(T, synsets1)
-    V2 = calculateVector(T, synsets2)
+    V1 = calculateVector(T, synsets1, method)
+    V2 = calculateVector(T, synsets2, method)
 
     similarity = 1 - spatial.distance.cosine(V1, V2)
 
 
-    print(V1)
-
-    print(V2)
-
-    print(str(similarity))
+    return similarity
 
 
 
@@ -96,15 +93,20 @@ def union(sentence1, sentence2):
 
     return list(set(sentence1) | set(sentence2))
 
-def calculateVector(T, S):
+def calculateVector(T, S, method):
 
     V = []
 
     for word in T:
 
-        if word in S:
-            V.append(1.0)
-        else:
-            V.append(getMaxSimilarityWup(word, S))
+        try:
+            if word in S:
+                V.append(1.0)
+            elif method == 'sim_path':
+                V.append(getMaxSimilarityPath(word, S))
+            elif method == 'wup':
+                V.append(getMaxSimilarityWup(word, S))
+        except:
+            V.append(0.0)
 
     return V
